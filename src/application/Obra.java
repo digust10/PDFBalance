53232e7d101d20de00275c58eec05bff5a4cc996
digust10 <ernest.diego@gmail.com>
2017-10-03 23:21:55 -0300

    criando a classe Obra basica 2

--- src/application/Obra.java
+++ src/application/Obra.java
@@ -1,5 +1,34 @@
 package application;
 
 public class Obra {
+	int id;
+	double custos;
+	String name;
 
+	public Obra(int id, double custos, String name) {
+		super();
+		this.id = id;
+		this.custos = custos;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getCustos() {
+		return custos;
+	}
+	public void setCustos(double custos) {
+		this.custos = custos;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
