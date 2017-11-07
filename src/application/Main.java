2d1bfb3cfaeb378b54ed529e83faa002eb52e51c
digust10 <ernest.diego@gmail.com>
2017-11-06 21:43:02 -0300

    acesso inicial. Select simples

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,21 +6,45 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
+import java.io.FileOutputStream; 
+import java.io.IOException; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.net.URISyntaxException; 
+import java.net.URL; 
+
+import java.io.File; 
+import java.util.ArrayList; 
+import com.almworks.sqlite4java.SQLiteConnection; 
+import com.almworks.sqlite4java.SQLiteConstants; 
+import com.almworks.sqlite4java.SQLiteException; 
+import com.almworks.sqlite4java.SQLiteStatement;
+	 
+
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		
 	}
 	
 	public static void main(String[] args) {
+		
+		System.out.println("printa");
+		try {
+		 SQLiteConnection db = new SQLiteConnection(new File("C:\\Users\\digust10\\Documents\\SQLiteBDteste\\test.db"));
+		 db.open(true);
+		 SQLiteStatement st = db.prepare("SELECT sum(custo) FROM ObraTable");
+		 if(st.step()){
+			 int x = st.columnInt(0);
+			 System.out.println("Eai?" + x);
+		 }
+		   // System.out.println("Eai?" + st.columnDouble(1));
+		} catch(Exception e) {
+			System.out.println("Nao");
+			e.printStackTrace();
+		}
 		launch(args);
 	}
+	
+
 }
